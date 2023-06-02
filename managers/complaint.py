b6a78b9d8bd80333ece94ac3a1d1f038548d05d2
Jaime De La Cruz <jaimeandres.dev@gmail.com>
2023-06-02 13:11:39 -0600

    delete complaint added

--- managers/complaint.py
+++ managers/complaint.py
@@ -7,7 +7,7 @@ class ComplaintManager:
         q = complaint.select()
         if user["role"] == RoleType.complainer:
             q = q.where(complaint.c.complainer_id == user["id"])
-        elif user ["user"] == RoleType.approver:
+        elif user ["role"] == RoleType.approver:
             q = q.where(complaint.c.state == State.pending)
         return await database.fetch_all(q)
     
@@ -16,4 +16,6 @@ class ComplaintManager:
         complaint_data["complainer_id"] = user["id"]
         id_ = await database.execute(complaint.insert().values(complaint_data))
         return await database.fetch_one(complaint.select().where(complaint.c.id == id_))
-    
+    @staticmethod
+    async def delete(complaint_id):
+        await database.execute(complaint.delete().where(complaint.c.id == complaint_id))
--- resources/complaint.py
+++ resources/complaint.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, dependencies, Depends
 from starlette.requests import Request
 from managers.complaint import ComplaintManager
-from managers.auth import oauth2_scheme, is_complainer
+from managers.auth import oauth2_scheme, is_complainer, is_admin
 from schemas.request.complaint import ComplaintIn
 from schemas.response.complaint import ComplaintOut
 from typing import List
@@ -18,3 +18,7 @@ async def get_complaints(request: Request):
 async def create_complaint(request: Request, complaint: ComplaintIn):
     user = request.state.user
     return await ComplaintManager.create_complaint(complaint.dict(), user)
+
+@router.delete("/complaints/{complaint_id}/",dependencies=[Depends(oauth2_scheme), Depends(is_admin)], status_code=204)
+async def delete_complaint(complaint_id: int):
+    await ComplaintManager.delete(complaint_id)
